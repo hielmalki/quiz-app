d9cd8c946f9971013741af2b221d49da6f8fa99c
Hicham El Malki <hi.elmalki@hotmail.com>
2024-08-22 19:56:03 +0200

    add keycloak-server

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faArrowLeft, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { useQuizStore } from "./store/store";
+import keycloak from './keycloak';
 
 // Font Awesome Icons zur Bibliothek hinzufügen
 library.add(faArrowLeft, faAngleLeft);
@@ -22,4 +23,17 @@ app.use(pinia);
 const quizStore = useQuizStore();
 quizStore.loadQuestions().then(() => {
     app.mount('#app');
+});
+
+keycloak.init({ onLoad: 'login-required' }).then((authenticated) => {
+    if (!authenticated) {
+        window.location.reload();
+    } else {
+        const app = createApp(App);
+        app.use(router);
+        app.provide('$keycloak', keycloak);
+        app.mount('#app');
+    }
+}).catch(() => {
+    console.log('Failed to initialize Keycloak');
 });
--- src/router/router.ts
+++ src/router/router.ts
@@ -3,11 +3,20 @@ import { createRouter, createWebHistory } from 'vue-router';
 import StartPage from '../components/StartPage.vue';
 import QuestionPage from '../components/QuestionPage.vue';
 import ResultPage from '../components/ResultPage.vue';
+import keycloak from '../keycloak';
+
+const requireAuth = (to, from, next) => {
+    if (keycloak.authenticated) {
+        next();
+    } else {
+        keycloak.login();
+    }
+};
 
 const routes = [
-    { path: '/', component: StartPage },
-    { path: '/question', component: QuestionPage },
-    { path: '/result', component: ResultPage },
+    { path: '/', component: StartPage, beforeEnter: requireAuth },
+    { path: '/question', component: QuestionPage, beforeEnter: requireAuth },
+    { path: '/result', component: ResultPage, beforeEnter: requireAuth },
 ];
 
 const router = createRouter({
